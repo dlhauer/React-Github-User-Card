39608e29fa36f5175a43e9fd29cfcfd18f8e2932
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2019-12-03 13:12:16 -0800

    API call

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import './App.css';
 import UsersList from './components/UsersList';
+import axios from 'axios';
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-      <UsersList/>
-    </div>
-  );
+
+
+class App extends React.Component {
+  
+  state = {
+    users: [],
+    userText: ''
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/dlhauer')
+      .then(response => {
+        // console.log(response.data);
+        this.setState({
+          users: [...this.state.users, response.data]
+        });
+        axios.get(response.data.followers_url)
+          .then(response => {
+            // console.log(this.state.users);
+            this.setState({
+              users: [...this.state.users, ...response.data]
+            });
+          })
+      })
+
+      // axios.get('https://api.github.com/users/dlhauer/followers')
+      //   .then(response => {
+      //     console.log(response.data);
+      //     this.setState({
+      //       users: [...users, response.data]
+      //     });
+      //   })
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <h1>App</h1>
+        <UsersList/>
+      </div>
+    );
+  }
 }
 
 export default App;
